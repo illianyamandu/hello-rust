7587d03d14d3982f6e6743067c15c5cd4308e8c0
Illian Yamandú <yamandu.developer@gmail.com>
2024-03-17 13:16:29 -0300

    refactor: removendo código inútil

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
 use std::io;
-use reqwest::blocking::{Client, ClientBuilder};
 
-fn main() {
-    let http_client = Client::new();
-    let result = http_client.get("http://localhost/tikmais-pdv-web/public/api/app/evento")
-        .send();
-
-    if result.is_ok() {
-        println!("Body: {:#?}", result.ok().unwrap().text().unwrap());
-        // let response = result.unwrap();
-        // let status = response.status();
-        // let body = response.text().unwrap();
-        // println!("Status: {}", status);
-        // println!("Body: {}", body);
-    } else {
-        println!("Error: {:?}", result.err());
-    }
-}
+fn main() {}
