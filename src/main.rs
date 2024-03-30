9b3da454c3759ae061252adb611549f87cc0865d
Illian Yamandú <yamandu.developer@gmail.com>
2024-03-30 16:23:45 -0300

    read the rust book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 use std::io;
 
-fn main() {}
+fn main() {
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
