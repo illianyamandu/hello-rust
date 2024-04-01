efeb416e8c4f1115bc4c0106da981edfc87ff36f
Illian Yamandú <yamandu.developer@gmail.com>
2024-03-31 21:02:17 -0300

    shadowing variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::io;
 
 fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    let x = 5;
+    let x = x + 1;
 
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
 }
